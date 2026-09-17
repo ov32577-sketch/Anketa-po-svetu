7d4e29d80be249f405286c15d77c74b12b461304
ov32577-sketch <ov32577@gmail.com>
2026-09-17 10:38:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
   Адрес заканчивается на /exec. Больше в этом файле ничего менять не нужно.
 */
 window.SURVEY_CONFIG = {
-  scriptUrl: ""
+  scriptUrl: "https://script.google.com/macros/s/AKfycbycQDldMNWwhJdzHsU1zniYyF4YjTDHA8kTJlBYbvg-7kcdFcYekTnSObTceTBTRF5hpw/exec"
 };
